f36815abf44fc029021fa7df0f28dfa6eb75a93d
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-12 16:32:17 +0500

    added more js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+// nav
+
 const mobileNavbar = document.getElementById("mobileNavbar");
 const closeNavButton = document.getElementById("closeNav");
 const openNavButton = document.querySelector(".mobile_menu");
+
+// tabs
+
+const tabCandidate = document.getElementById("tab-candidate");
+const tabCompany = document.getElementById("tab-company");
+
+// Navbar functions
 
 openNavButton.addEventListener("click", () => {
     mobileNavbar.style.left = "0"
@@ -8,4 +17,16 @@ openNavButton.addEventListener("click", () => {
 
 closeNavButton.addEventListener("click", () => {
     mobileNavbar.style.left = "-200%"
+})
+
+// tab functions
+
+tabCompany.addEventListener("click", () => {
+    tabCompany.classList.add("active")
+    tabCandidate.classList.remove("active")
+})
+
+tabCandidate.addEventListener("click", () => {
+    tabCandidate.classList.add("active");
+    tabCompany.classList.remove("active");
 })
